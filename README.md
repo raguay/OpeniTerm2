8f210df8f63158ef9bb44d82363ac70c94e10def
Richard Guay <raguay@customct.com>
2017-02-07 17:49:35 +0700

    Initial

---
+++ README.md
@@ -0,0 +1,15 @@
+## OpeniTerm2
+
+Plugin for [fman.io](https://fman.io) to open the current directory in [iTerm2](https://www.iterm2.com/). This overrides the built-in function for opening the directory in Terminal.
+
+Install by uploading "OpeniTerm2" to your [data directory](https://fman.io/docs/customizing-fman)`/Plugins`.
+
+After restarting **fman**, you will be able to open the current directory in [iTerm2](https://www.iterm2.com/).
+
+### Usage
+
+Pressing **shift-o** will open the current directory in [iTerm2](https://www.iterm2.com/).
+
+### Features
+
+ - Opens current directory in [iTerm2](https://www.iterm2.com/).
---
+++ openiterm2/__init__.py
@@ -0,0 +1,15 @@
+from fman import DirectoryPaneCommand, show_alert, load_json, DATA_DIRECTORY
+import os
+
+class OpenIterm2(DirectoryPaneCommand):
+    def __call__(self):
+        selected_files = self.pane.get_selected_files()
+        if len(selected_files) >= 1 or (len(selected_files) == 0 and self.get_chosen_files()):
+            if len(selected_files) == 0 and self.get_chosen_files():
+                selected_files.append(self.get_chosen_files()[0])
+            dirName = selected_files[0]
+            if os.path.isfile(dirName):
+                dirName = os.path.dirname(dirName)
+            os.system("/usr/bin/osascript '" + DATA_DIRECTORY + "/Plugins/OpeniTerm2/OpeniTerm2.scpt' '" + dirName + "'")
+        else:
+            show_alert("No directory selected")
