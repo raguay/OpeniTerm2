852468f666b8d6cb5de5e53a971435ee36ba5e25
Richard Guay <raguay@customct.com>
2017-04-17 15:38:30 +0700

    Fixed for plugin directory structure changes.

--- openiterm2/__init__.py
+++ openiterm2/__init__.py
@@ -10,6 +10,7 @@ class OpenIterm2(DirectoryPaneCommand):
             dirName = selected_files[0]
             if os.path.isfile(dirName):
                 dirName = os.path.dirname(dirName)
-            os.system("/usr/bin/osascript '" + DATA_DIRECTORY + "/Plugins/OpeniTerm2/OpeniTerm2.scpt' '" + dirName + "'")
+            show_alert(DATA_DIRECTORY)
+            os.system("/usr/bin/osascript '" + DATA_DIRECTORY + "/Plugins/Third-party/OpeniTerm2/OpeniTerm2.scpt' '" + dirName + "'")
         else:
             show_alert("No directory selected")
