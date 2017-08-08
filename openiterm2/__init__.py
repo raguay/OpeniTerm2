1993e07191b1feb221abb62d0b356763589813c2
Richard Guay <raguay@customct.com>
2017-08-08 15:40:26 -0400

    Fixing Versioning.

--- openiterm2/__init__.py
+++ openiterm2/__init__.py
@@ -1,4 +1,4 @@
-from fman import DirectoryPaneCommand, show_alert, load_json, DATA_DIRECTORY
+from fman import DirectoryPaneCommand, show_alert, DATA_DIRECTORY, FMAN_VERSION
 import os
 
 class OpenIterm2(DirectoryPaneCommand):
@@ -10,7 +10,9 @@ class OpenIterm2(DirectoryPaneCommand):
             dirName = selected_files[0]
             if os.path.isfile(dirName):
                 dirName = os.path.dirname(dirName)
-            show_alert(DATA_DIRECTORY)
-            os.system("/usr/bin/osascript '" + DATA_DIRECTORY + "/Plugins/Third-party/OpeniTerm2/OpeniTerm2.scpt' '" + dirName + "'")
+            if FMAN_VERSION > 4:
+                os.system("/usr/bin/osascript '" + DATA_DIRECTORY + "/Plugins/Third-party/OpeniTerm2/OpeniTerm2.scpt' '" + dirName + "'")
+            else:
+                os.system("/usr/bin/osascript '" + DATA_DIRECTORY + "/Plugins/OpeniTerm2/OpeniTerm2.scpt' '" + dirName + "'")
         else:
             show_alert("No directory selected")
