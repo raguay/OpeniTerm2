34cbc18dd792da737124420dde51f1209e426a40
Richard Guay <raguay@customct.com>
2017-10-05 12:09:18 -0400

    Added new tab

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## OpeniTerm2
 
-Plugin for [fman.io](https://fman.io) to open the current directory in [iTerm2](https://www.iterm2.com/). This overrides the built-in function for opening the directory in Terminal.
+Plugin for [fman.io](https://fman.io) to open the current directory in [iTerm2](https://www.iterm2.com/). This overrides the built-in function for opening the directory in Terminal. You can set a preference for always opening in a new tab or not. If you change the preference file, you have to reload fman to take effect.
 
 Install by uploading "OpeniTerm2" to your [data directory](https://fman.io/docs/customizing-fman)`/Plugins`.
 
--- openiterm2/__init__.py
+++ openiterm2/__init__.py
@@ -1,8 +1,16 @@
-from fman import DirectoryPaneCommand, show_alert, DATA_DIRECTORY, FMAN_VERSION
+from fman import DirectoryPaneCommand, show_alert, DATA_DIRECTORY, FMAN_VERSION, load_json, save_json
 import os
 
 class OpenIterm2(DirectoryPaneCommand):
     def __call__(self):
+        ntab = False
+        loc = load_json('OpenIterm2.json')
+        if loc is None:
+            jloc = dict()
+            jloc['NewTab'] = False
+            save_json('OpenIterm2.json',jloc)
+        else:
+            ntab = loc['NewTab']
         fmanv = 0
         if isinstance(FMAN_VERSION, str):
             fmanvParts = FMAN_VERSION.split('.')
@@ -18,8 +26,14 @@ class OpenIterm2(DirectoryPaneCommand):
             if os.path.isfile(dirName):
                 dirName = os.path.dirname(dirName)
             if fmanv > 4:
-                os.system("/usr/bin/osascript '" + DATA_DIRECTORY + "/Plugins/Third-party/OpeniTerm2/OpeniTerm2.scpt' '" + dirName + "'")
+                if ntab:
+                    os.system("/usr/bin/osascript '" + DATA_DIRECTORY + "/Plugins/Third-party/OpeniTerm2/OpeniTerm2-newtab.scpt' '" + dirName + "'")
+                else:
+                    os.system("/usr/bin/osascript '" + DATA_DIRECTORY + "/Plugins/Third-party/OpeniTerm2/OpeniTerm2.scpt' '" + dirName + "'")
             else:
-                os.system("/usr/bin/osascript '" + DATA_DIRECTORY + "/Plugins/OpeniTerm2/OpeniTerm2.scpt' '" + dirName + "'")
+                if ntab:
+                    os.system("/usr/bin/osascript '" + DATA_DIRECTORY + "/Plugins/OpeniTerm2/OpeniTerm2-newtab.scpt' '" + dirName + "'")
+                else:
+                    os.system("/usr/bin/osascript '" + DATA_DIRECTORY + "/Plugins/OpeniTerm2/OpeniTerm2.scpt' '" + dirName + "'")
         else:
             show_alert("No directory selected")
